4cab7b353cc4e86f5a677b1235a992d61cc10770
Meedoeed <skykk27@gmail.com>
2025-12-16 20:53:10 +0300

    added loggingMiddleware

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		log.Printf(
+			"[INFO] %s - %s - %s",
+			r.Method,
+			r.URL.Path,
+			time.Since(start),
+		)
+	})
+}
 
 func runServer(addr string, handler http.Handler) error {
 	return http.ListenAndServe(addr, handler)
@@ -20,7 +36,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HomeHandler)
-	mux.HandleFunc("/hello", HelloHandler)
+	mux.Handle("/hello", LoggingMiddleware(http.HandlerFunc(HelloHandler)))
 	err := runServer(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
